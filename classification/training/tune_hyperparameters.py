2e0ce616bef68ae9993b9a09af0b8d4bafa9885d
Dropptimus <emillundin9@gmail.com>
2025-02-25 09:27:00 +0100

    fixed project root

--- classification/training/tune_hyperparameters.py
+++ classification/training/tune_hyperparameters.py
@@ -1,14 +1,15 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)  # Use insert(0, ...) to prioritize it
+    
 import ray
 from ray import train, tune
 from ray.train import Checkpoint, RunConfig
 from ray.tune.schedulers import ASHAScheduler
 import torch
 import json
-import glob
 import tempfile
 import numpy as np
 from torch.utils.data import DataLoader
