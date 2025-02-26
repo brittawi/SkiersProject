a2e0a7be76b93f2ca60327930e7e3b066384e57b
Dropptimus <emillundin9@gmail.com>
2025-02-26 11:43:52 +0100

    Clean up, added alphapose, more config

--- feedback_system/feedback_system.py
+++ feedback_system/feedback_system.py
@@ -11,6 +11,7 @@ from utils.nets import LSTMNet, SimpleMLP
 from utils.config import update_config
 from utils.split_cycles import split_into_cycles
 from utils.preprocess_signals import *
+from alphapose.scripts.demo_inference import run_inference
 
 import torch
 import numpy as np
@@ -30,6 +31,8 @@ video_path = r"C:\awilde\britta\LTU\SkiingProject\SkiersProject\Data\selectedDat
 def main():
     # TODO put in config file?!
     # Load config 
+    
+    # TODO Change
     print("Loading config...")
     cfg = update_config("./classification/training/config.yaml") 
     
@@ -38,6 +41,11 @@ def main():
     print(f"Device = {device}")
     
     # Step 1: Get Keypoints from AlphaPose TODO
+    print("Loading config...")
+    run_args = update_config("./feedback_system/pipe_test.yaml") # TODO Testing set up fix for full pipeline
+    output_path, results_list = run_inference(run_args)
+    # TODO Convert output to similar to CVAT?
+
     
     # Step 2: Split into cycles
     input_data = []
@@ -165,32 +173,9 @@ def main():
         
         dtw_comparisons = compare_selected_cycles(expert_data, cycle, joints, INPUT_VIDEO, video_path, visualize=True)
 
-    # Step 4: Based on classification use DTW
+        # Step 5: Give feedback
+ 
     
-    # # Load cycle data
-    # #id = "15_cut" # r->l gear 3, cycle 5
-    # id = "22_cut" # l->r gear 3, cycle 5
-    # #id = "38" # front gear 3, cycle 5
-    # file_path = "./data/labeled_data/labeled_cycles_" + id + ".json"
-    # cycle_to_compare = "Cycle 5"
-    # user_video = r"C:\awilde\britta\LTU\SkiingProject\SkiersProject\Data\selectedData\DJI_00" + id + ".mp4"  # Path to the corresponding video file
-    # data = load_json(file_path)
-    # user_data = data.get(cycle_to_compare)
-    # print(user_data)
-    
-    # # Load expert data
-    # expert_path = "./data/expert_data/expert_cycles_gear3.json"
-    # expert_data = load_json(expert_path)
-    
-    # video_path = r"C:\awilde\britta\LTU\SkiingProject\SkiersProject\Data\selectedData"
-
-    # # Define joint triplets for angle comparisons
-    # # TODO to compare cycles we can either input joint triplets, then we need to set use_keypoints to false
-    # # otherwise we can input joints, then it will use raw keypoints for DTW
-    # joint_triplets = [("RHip", "RKnee", "RAnkle"), ("LHip", "LKnee", "LAnkle"), ("RShoulder", "RElbow", "RWrist"), ("LShoulder", "LElbow", "LWrist")]
-    # joints = ["RHip", "RKnee", "RAnkle", "LHip", "LKnee", "LAnkle", "RShoulder", "RElbow", "RWrist", "LShoulder", "LElbow", "LWrist"]
-    
-    # dtw_comparisons = compare_selected_cycles(expert_data, user_data, joints, user_video, video_path, visualize=False)
     
 
 if __name__ == '__main__':
