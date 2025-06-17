545fcacb94c08a18fc927a4ca4b608f059a7991b
Dropptimus <emillundin9@gmail.com>
2025-06-17 15:47:38 +0200

    removed commented code and fix file name

--- classification/classify_angle/classify_angle_test.py
+++ classification/classify_angle/classify_angle_test.py
@@ -2,12 +2,12 @@ import os
 import sys
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 if project_root not in sys.path:
-    sys.path.insert(0, project_root)  # Use insert(0, ...) to prioritize it
+    sys.path.insert(0, project_root)
 from utils.load_data import load_json
 from utils.classify_angle import classify_angle
 
 """
-This is just a test file to chedk if the classify_angle function works as expected.
+This is just a test file to check if the classify_angle function works as expected.
 ANNO_DIR: Directory where the annotations are stored, direct output from AlphaPose or annotated.
 video_ids: List of tuples with video_id and the expected label (Front, Left, Right).
 """
@@ -15,9 +15,6 @@ video_ids: List of tuples with video_id and the expected label (Front, Left, Rig
 def classify():
     ANNO_DIR = "e:/SkiProject/annotations_test_DJI_0044/After_Mixed_level_output/coco_json"
     video_ids = [(58, "Front"), (59, "Front"), (62, "Front"), (63, "Front"), (64, "Right"), (65, "Left"), (68, "Right"), (72, "Left"), (75, "Left"), (84, "Front"), (87, "Right"), (92, "Front"), (94, "Left"), (113, "Right"), (127, "Left"), (128, "Right")]
-    #video_id ="68"
-    #video_ids = [(58, "Front"), (59, "Front"), (62, "Front"), (63, "Front")]
-    #video_ids = [(87, "Right")]
     correct = 0
     for video_id in video_ids:
         file_path = os.path.join(ANNO_DIR, f"DJI_{int(video_id[0]):04d}_coco.json")
